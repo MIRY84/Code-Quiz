18b5a8f1d4ebad65bfba714f034cbca093b11b13
Mirella <mirella.dimichino@gmail.com>
2023-01-14 12:42:20 +0000

    added questions and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var  questions = [
+  
+    {
+      title : "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "number"],
+      correctAnswer: "alerts"
+    },
+
+    {
+      title: "The condition in an if / else statement is enclosed within _________.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      correctAnswer: "curly brackets"
+    },
+
+    {
+      title: "Arrays in JavaScript can be used to store ________.",
+      choices : ("numbers and strings", "other arrays", "booleans", "all of the above"),
+      correctAnswer: "all the above"
+    },
+
+    {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices : ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      correctAnswer : "console.log"
+    },
+
+    {
+      title: "String values must be enclosed within ______ when being assigned to variables.",
+      choice: ["commas", "curly brackets", "quotes", "parentheses"],
+      correctAnswer: "quotes"
+    }
+  ];
